429b12a58cafc6de84d7f087904ab216f9bff299
Camden Wagner <awesomemancam@gmail.com>
2015-05-11 20:51:58 -0300

    All base Eclipse files necessary for core dev

---
+++ Cryptography_Project/src/main/Decrypter.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Decrypter {
+	
+	public static String d_shiftCipher(String input, int i) {
+		String decryption = "";
+		//shift i spaces up the alphabet
+		
+		return null;
+	}
+
+}
