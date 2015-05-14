f56f026467547239a217d0332b56c0ba705babfb
Camden Wagner <camden.i.wagner@ivarcode.net>
2015-05-14 11:40:06 -0400

    completed decryption of Wagstein cipher

--- Cryptography_Project/src/main/Decrypter.java
+++ Cryptography_Project/src/main/Decrypter.java
@@ -3,14 +3,14 @@ package main;
 import java.util.Scanner;
 
 public class Decrypter {
-	
+
 	private static String decryption;
-	
+
 	public static String decrypt(String message, int cipherType) {
 		String decodedMessage = null;
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
-		
+
 		if (cipherType == 1) {
 			System.out.print("Shift number (no greater than 26)\n  :: ");
 			int i = scanner.nextInt();
@@ -19,12 +19,12 @@ public class Decrypter {
 			// WAGSTEIN CIPHER
 			System.out.print("Enter the value of x (1-10)\n  :: ");
 			int i = scanner.nextInt();
-			decodedMessage = d_wagstein(message, i);
+			decodedMessage = d_wagstein(message+" ", i);
 		}
-		
+
 		return decodedMessage;
 	}
-	
+
 	public static String d_shiftCipher(String message, int i) {
 		String decryption = "";
 		int[] bytes = new int[message.length()];
@@ -40,10 +40,10 @@ public class Decrypter {
 		for (int k = 0; k < message.length(); k++) {
 			decryption += (char)bytes[k];
 		}
-		
+
 		return decryption;
 	}
-	
+
 	public static String d_wagstein(String message, int x) {
 		decryption = "";
 		int[] value = new int[message.length()];
@@ -56,31 +56,41 @@ public class Decrypter {
 				value[k] = (int)message.charAt(k)-97;
 			} else if ((int)message.charAt(k) > 64 && (int)message.charAt(k) < 75) {
 				value[k] = (int)message.charAt(k)-65;
+			} else if ((int)message.charAt(k) == 47) {
+				value[k] = 47; //space
 			} else {
-				value[k] = 32; //space
+				value[k] = 32;
 			}
 		}
 		int sum = 0;
 		int i = 0;
 		//compress sequential numbers 0-9 to multiple-digit numbers
 		for (int j = 0; j < value.length; j++) {
-			sum = 0;
 			if (value[j] == 32) {
-				//do nothing
+				bytes[i] = sum;
+				i++;
+				sum = 0;
+			} else if (value[j] == 47) {
+				bytes[i] = 0;
 			} else {
-				while (value[j] != 32) {
-					sum *= 10;
-					sum += value[j];
-					j++;
-				}
+				sum *= 10;
+				sum += value[j];
 			}
-			bytes[i] = sum;
-			i++;
 		}
 		//turn into words by dividing by x^nth letter in word and then dividing again by the nth-1 letter
 		//adding to return String
-		
-		
+		for (int m = 0; m < bytes.length; m++) {
+			if (bytes[m] == 0) {
+				coeff[m] = 32;
+				powerCounter = 1;
+			} else {
+				int pow = powerCounter-1;
+				coeff[m] = (int)(bytes[m]/(Math.pow(x, pow)*powerCounter));
+				powerCounter++;
+				coeff[m] += 64;
+			}
+			decryption += (char)coeff[m];
+		}
 		return decryption;
 	}
 
