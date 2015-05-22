c6d069a1259681b4ff99fe12291237db7538b03d
Camden Wagner <awesomemancam@gmail.com>
2015-05-22 09:13:53 -0400

    vigenere crack work

--- Cryptography_Project/src/main/Cracker.java
+++ Cryptography_Project/src/main/Cracker.java
@@ -10,8 +10,7 @@ public class Cracker {
 			//TODO
 			System.out.print("Currently not in function.");
 		} else if (cipherType == 3) {
-			//TODO
-			System.out.print("Currently not in function.");
+			c_vigenère(codedMessage);
 		} else {
 			System.out.print("Invalid choice.");
 		}
@@ -32,6 +31,42 @@ public class Cracker {
 			}
 		}
 		System.out.println("\nBest Decryption with " + bestMatch + " matching words:\n  :: " + bestDecryption);
+	}
+	
+	public static void c_vigenère(String codedMessage) {
+		System.out.println();
+		String currWord = "a";
+		String bestMatch = "";
+		int x = 0;
+		int bestNum = 0;
+		for (int i = 0; i < Dictionary.dictionary.size(); i++) {
+			x = scanForWords(Decrypter.d_vigenère(codedMessage, Dictionary.dictionary.get(i)));
+			if (x > bestNum) {
+				bestMatch = Decrypter.d_vigenère(codedMessage, Dictionary.dictionary.get(i));
+			}
+		}
+		System.out.println("Best decryption from dictionary:  " + bestMatch + "\nTesting all non-existent 3 letter words.");
+		while (true) {
+			x = scanForWords(Decrypter.d_vigenère(codedMessage, currWord));
+			if (x > bestNum) {
+				bestMatch = Decrypter.d_vigenère(codedMessage, currWord);
+			}
+			
+			if (currWord.charAt(currWord.length()-1) == 'z') {
+				currWord += "a";
+			} else {
+				@SuppressWarnings("unused")
+				String newWord = "";
+				for (int i = 0; i < currWord.length()-1; i++) {
+					newWord += currWord.charAt(i);
+					newWord += (char)((int)currWord.charAt(currWord.length())+1);
+				}
+			}
+			if (currWord.length() == 4) {
+				System.out.println("More than 3 characters");
+				break;
+			}
+		}
 	}
 	
 	public static int scanForWords(String message) {
